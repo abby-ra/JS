f3e019e9c7a88c487f7e273e415400618acae0fb
abby-ra <abbynayaraj@gmail.com>
2024-12-30 22:55:42 +0530

    statment.js added

--- js/statements.js
+++ js/statements.js
@@ -1,11 +1,21 @@
 //if statment
 
 let viewer="abby is good";
+let customerIsBanned = false
+let finance = true
 let reply;
-if(viewer){
+
+if(customerIsBanned){
+    reply = `sorry`
+}
+else if(viewer && finance){
     reply = `Enjoy ${viewer}`;
+}
+else if(viewer){
+    reply = `Enjoy`
 }
 else{
     reply = `sorry I am still working`
 }
 console.log(reply)
+
