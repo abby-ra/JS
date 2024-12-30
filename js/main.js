81c1db7b861d19e2a322e702eca19599fbd8f96c
abby-ra <abbynayaraj@gmail.com>
2024-12-30 22:47:02 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+console.log("Hello Abby")
+console.log(typeof 2)
+
+const myVar="Abby"
+console.log(myVar.length)
+console.log(myVar.charAt(2))
+console.log(myVar.indexOf("y"))
+console.log(myVar.lastIndexOf("b"))
+console.log(myVar.toUpperCase())
+console.log(myVar.includes("bb"))
+console.log(myVar.split("b"))
+console.log("Abby,is,Good".split(""))
+
+const myNum =  42
+console.log(myNum == "42")
+console.log(myNum)
+console.log(Number(myNum) + 2)
+console.log(Number(true))
+
+const myfloat = "42.01aaa"
+console.log(Number.isInteger(myfloat))
+console.log(Number.parseFloat(myfloat))
+console.log(Number.parseInt(myfloat).toFixed(5))
+console.log(myfloat.toString())
+
+console.log(Math.pow(10,2))
+console.log(Math.min(10,2))
+console.log(Math.floor( Math.random() * 100 + 1))
+
+const name="abby"
+console.log("abbyisnot an ordinarygirl".charAt((Math.floor(Math.random()*10 ) +1)))
+
---
+++ js/statements.js
@@ -0,0 +1,11 @@
+//if statment
+
+let viewer="abby is good";
+let reply;
+if(viewer){
+    reply = `Enjoy ${viewer}`;
+}
+else{
+    reply = `sorry I am still working`
+}
+console.log(reply)
